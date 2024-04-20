ed8936cd764c2296718c2ace6cf537cfea4e48a7
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-04-20 15:13:24 +0400

    connected screen section_3 to context, now it gets information from context and sets new information to storage after leaving from the screen

--- examinee/screens/section_3/Section_3.tsx
+++ examinee/screens/section_3/Section_3.tsx
@@ -2,28 +2,62 @@ import { StyleSheet, Text, View } from "react-native";
 import { Section_3Types } from "../../types/components";
 import Form from "../../components/Form";
 import data from "../../data/section_3.json"
-import { useState } from "react";
-import { ChooseExerciseType } from "../../types/global";
+import { useEffect, useRef, useState } from "react";
+import { ChooseExerciseType, SectionInfoType } from "../../types/global";
 import GoToNextButton from "../../components/GoToNextButton";
-import { RANDOM_MODE } from "../../constants/modes";
+import { PROGRESS_MODE, RANDOM_MODE } from "../../constants/modes";
 import { getRandomInt } from "../../utils/global";
+import { setItem } from "../../utils/storage";
+import { SEC_3 } from "../../constants/sections";
+import { useSections } from "../../contexts/SectionsProvider";
 
-export default function Section_3({ navigation, route }: Section_3Types){
+export default function Section_3({ navigation, route }: Section_3Types){    
     const exercises: ChooseExerciseType[] = data
-    const [currentExercise, setCurrentExercise] = useState(route.params.mode === RANDOM_MODE ? exercises[getRandomInt(0,900)] : exercises[899])
+    const sectionsContext = useSections()
+    if(sectionsContext === null){
+        return
+    }
+    const { sectionsData, changeSectionsData } = sectionsContext
+    const sectionData = sectionsData[SEC_3] ? sectionsData[SEC_3] : {reachedNumber : 0}
+    const [currentExercise, setCurrentExercise] = useState(route.params.mode === RANDOM_MODE ? exercises[getRandomInt(0,900)] : exercises[sectionData.reachedNumber])
     const [answered, setAnswered] = useState<false | string>(false)
+    const statsRef = useRef({combo : 0, count: 0}).current
+    
+
+    useEffect(()=>{
+        const saveProgress = async ()=>{                        
+            if(route.params.mode === PROGRESS_MODE){
+                await setItem(SEC_3, sectionData)                
+            }
+        }
+
+        navigation.addListener("beforeRemove", saveProgress)
+
+        return ()=> navigation.removeListener("beforeRemove", saveProgress)
+    }, [sectionData])
 
     const onChoose = (variant: string)=>{
+        statsRef.count++
         setAnswered(variant)
+        if(route.params.mode === PROGRESS_MODE && sectionData.reachedNumber <= 900){
+            changeSectionsData(SEC_3, {...sectionData, reachedNumber: sectionData.reachedNumber+1})
+        }else if(route.params.mode === PROGRESS_MODE){
+            changeSectionsData(SEC_3, {...sectionData, reachedNumber: 0})
+        }
     }
 
     const onGoToNext = ()=>{
         setAnswered(false)
-        setCurrentExercise(route.params.mode === RANDOM_MODE ? exercises[getRandomInt(0,900)] : exercises[100])
+        setCurrentExercise(route.params.mode === RANDOM_MODE ? exercises[getRandomInt(0,900)] : exercises[sectionData.reachedNumber])
     }
 
     return (
         <>
+            <Text>{route.params.mode === PROGRESS_MODE ? "level: " : "count: "}{route.params.mode === PROGRESS_MODE ? sectionData.reachedNumber : statsRef.count}</Text>
+            <View>
+                <Text>repository: {currentExercise.repository}</Text>
+                <Text>exercise: {currentExercise.number}</Text>
+            </View>
             {answered && <View style={styles.goToNextButtonBackground}>
                 <GoToNextButton onGoToNext={onGoToNext}/>
             </View>}
