a2a7c3b9310214fae90d32c81133be23134c08df
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-04-30 19:16:36 +0400

    added Section 8 logic and recovered data

--- examinee/screens/section_8/Section_8.tsx
+++ examinee/screens/section_8/Section_8.tsx
@@ -1,8 +1,42 @@
-import { Text, View } from "react-native";
 import { Section_8Types } from "../../types/components";
+import Choose from "../../components/Choose";
+import data from "../../data/section_8.json"
+import useChoose from "../../hooks/useChoose";
+import { ChooseExerciseType, SentenceExerciseType } from "../../types/global";
+import { SEC_8 } from "../../constants/sections";
+import GoToNextButton from "../../components/GoToNextButton";
+import Banner from "../../ads/Banner";
 
-export default function Section_8({  }: Section_8Types){
+export default function Section_8({ navigation, route }: Section_8Types){
+    const equipment = useChoose<SentenceExerciseType>({data, section : SEC_8, mode : route.params.mode, navigation})
+    if(!equipment){
+        return
+    }
+    const {sectionData, currentExercise, answered, statsRef, onChoose, onGoToNext} = equipment
+    const exercise: ChooseExerciseType = {
+        ...currentExercise,
+        variants : [{
+            text: "The senctence is correct",
+            id : "true",
+            variant : "true"
+        }, {
+            text : "The senctence is incorrect",
+            id : "false",
+            variant : "false"
+        }]
+    }
     return (
-        <Text>asdasd</Text>
+        <>
+            <Choose
+                exercise={exercise}  
+                sectionData={sectionData}  
+                mode={route.params.mode}
+                stats={statsRef}
+                answered={answered}
+                onChoose={onChoose}
+            />
+            {answered && <GoToNextButton onGoToNext={onGoToNext}/>}
+            <Banner/>
+        </>
     )
 }
