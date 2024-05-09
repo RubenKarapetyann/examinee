f75311a57a32b00c2bda62eca45c865031194956
RubenKarapetyann <ruben.karapetyannn@gmail.com>
2024-05-09 23:06:34 +0400

    added logic on Screen Saved

--- examinee/screens/saved/Saved.tsx
+++ examinee/screens/saved/Saved.tsx
@@ -1,7 +1,89 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { ChooseExerciseType, SaveType, SectionNames, SentenceExerciseType } from "../../types/global";
+import SavedCard from "../../components/SavedCard";
+import Choose from "../../components/Choose";
+import { PROGRESS_MODE } from "../../constants/modes";
+import { useSaves } from "../../contexts/SavesProvider";
+import { SavedProps } from "../../types/components";
+import SaveButton from "../../components/SaveButton";
+import Button from "../../components/Button";
+import Icon from "react-native-vector-icons/FontAwesome"
 
-export default function Saved(){
+
+export default function Saved({ navigation }: SavedProps){
+    const [choseExercise, setChoseExercise] = useState<false | ChooseExerciseType | SentenceExerciseType>(false)
+    const savesContext = useSaves()
+    if(!savesContext){
+        return
+    }
+    const { saves } = savesContext
+
+    const chooseHandle = (exercise: SaveType)=> {
+        setChoseExercise(exercise)
+        navigation.setOptions({
+            headerRight: ()=> SaveButton<ChooseExerciseType | SentenceExerciseType>({exercise, section : exercise.section})
+        })
+    }
+    const backHandle = ()=> {
+        setChoseExercise(false)
+        navigation.setOptions({
+            headerRight: ()=> <Icon name="bookmark" size={30} style={{marginRight : 30}}/>
+        })
+    }
+
     return (
-        <Text>saved</Text>
+        <View>
+            {choseExercise ? 
+            <>
+                <Choose
+                    exercise={"variants" in choseExercise ? choseExercise : {...choseExercise, 
+                        variants : [{
+                            text: "The senctence is correct",
+                            id : "true",
+                            variant : "true"
+                        }, {
+                            text : "The senctence is incorrect",
+                            id : "false",
+                            variant : "false"
+                        }]  
+                    }}  
+                    sectionData={{reachedNumber : -1}}  
+                    mode={PROGRESS_MODE}
+                    stats={{ combo: 0, count : 0 }}
+                    answered={choseExercise.correct_answer}
+                    onChoose={()=>{}}
+                />
+                <View style={styles.buttonContainer}><Button icon="arrow-left" width={75} height={75} handle={backHandle}/></View>
+            </> : 
+            <View style={styles.container}><ScrollView style={styles.scroll}>{saves.map(saved=><SavedCard
+                key={saved.id}
+                section={saved.section}
+                exercise={saved}
+                handle={chooseHandle}
+            />)}</ScrollView></View>}
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container : {
+        width : "100%",
+        height : "100%",
+        padding : 10,
+        gap : 10,
+        alignItems : "center",
+    },
+    scroll : {
+        width : "100%",
+        padding : 10,
+    },
+    buttonContainer : {
+        position : "absolute",
+        width : "100%",
+        height : "100%",
+        alignItems : "center",
+        justifyContent : "flex-end",
+        bottom : 50
+    }
+})
